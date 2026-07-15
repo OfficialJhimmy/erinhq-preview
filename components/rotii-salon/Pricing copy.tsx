0643f24b5d6e9ded9505718dfed24915b61e1a64
Feyijimi Erinle <erin@Feyijimis-MacBook-Air.local>
2026-07-15 07:24:25 +0100

    fix:added typescript interface

--- components/rotii-salon/Pricing copy.tsx
+++ components/rotii-salon/Pricing copy.tsx
@@ -5,7 +5,7 @@ import { motion, useReducedMotion } from "framer-motion";
 import PricingSidebar from "./PricingSidebar";
 import PricingCard from "./PricingCard";
 import PricingImage from "./PricingImage";
-import { priceCategories } from "./data";
+import { priceCategories } from "./pricingData";
 
 const GRAIN_SVG =
   "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='200' height='200'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='2' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E";
